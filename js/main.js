c45341724a3d2ed80248671d2db61fa2e34c8e37
CharlieGoodson <max.sadchikov@gmail.com>
2018-01-10 23:10:56 +0200

    final commit

--- js/main.js
+++ js/main.js
@@ -2,8 +2,12 @@
 
 const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
 const HIDDEN_DETAIL_CLASS = 'hidden-detail';
+const TINY_EFFECT_CLASS = 'is-tiny';
+
 const ESC_KEY = 27;
 
 function imageFromThumb(thumbnail) {
@@ -45,7 +49,12 @@ function hideDetail() {
 }
 
 function showDetails() {
+    const frame = document.querySelector(DETAIL_FRAME_SELECTOR);
     document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+    setTimeout(() => {
+        frame.classList.remove(TINY_EFFECT_CLASS);
+    }, 50);
 }
 
 function addKeyPressHandler() {
